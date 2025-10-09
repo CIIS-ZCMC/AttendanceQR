3e1913e3c44c857ca69df440a4192e1bcdf12f0e
ZCMCCaimor2 <zcmc.caimor@gmail.com>
2025-10-09 15:07:20 +0800

    fix flex on status

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -14,14 +14,14 @@ class AttendanceController extends Controller
 {
     public function validateLocation(Request $request)
     {
-        $geofenceCenter = ['lat' => 6.905891, 'lng' => 123.080778];
+        $geofenceCenter = ['lat' => 6.905891, 'lng' => 122.080778];
         $geofenceRadius = 40; // meters
 
         $userLat = $request->lat;
         $userLng = $request->lng;
 
-        // $userLat = 8.905835;
-        // $userLng = 122.080778;
+        $userLat = 6.905835;
+        $userLng = 122.080778;
 
         /**
          * Add Validation here soon , that active attendance does not need location based.
--- resources/js/Pages/Scan/status/RecordedStatus.jsx
+++ resources/js/Pages/Scan/status/RecordedStatus.jsx
@@ -1,28 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import check from '../../../src/check.gif'
-import { recordedStatusContants } from '@/constants/contants';
+import check from "../../../src/check.gif";
+import { recordedStatusContants } from "@/constants/contants";
 
 const RecordedStatus = () => {
-
     const { header, description } = recordedStatusContants;
 
     return (
-        <div>
-            <img
-                src={check}
-                alt=""
-                width="40px"
-                height="40px"
-            />
-            <h2 className="text-md font-semibold text-gray-800">
-                {header}
-            </h2>
-            <p className="text-sm text-gray-500 text-center">
-                {description}
-            </p>
+        <div className="flex flex-col items-center justify-center gap-3">
+            <img src={check} alt="" width="40px" height="40px" />
+            <h2 className="text-md font-semibold text-gray-800">{header}</h2>
+            <p className="text-sm text-gray-500 text-center">{description}</p>
         </div>
-    )
-}
+    );
+};
 
-export default RecordedStatus
+export default RecordedStatus;
